45a155862a850e3eade0ca4cd0044ece141c3467
Pete Jeffryes <pjeffryes@tendrilinc.com>
2017-12-03 17:18:12 -0700

    title and some new colors

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      itemsPerRow: 4,
-      verticalMargin: 3,
-      horizontalMargin: 3,
+      itemsPerRow: 9,
+      verticalMargin: 4,
+      horizontalMargin: 4,
       radius: 5,
       pathColor: colors.paths
     }
@@ -30,25 +30,36 @@ class App extends Component {
   render() {
 
     const containerStyle = {
-      margin: '5% auto',
+      margin: 'auto',
       display: 'flex',
       flexFlow: 'row wrap',
       justifyContent: 'flex-start',
       maxWidth: '500px'
     }
 
+    const titleContainer = {
+      width: '100%',
+      color: colors.title,
+      position: 'absolute',
+      textShadow: `${colors.titleShadow} -4px 3px 2px`,
+      backgroundColor: 'rgba(260, 260, 264, 0.6)'
+    }
+
     return (
-      <div className="App" style={containerStyle} >
-        <Canvas 
-          id='svg' 
-          x='-50' 
-          y='-60' 
-          width='100'
-          height='140' 
-          gridProperties={this.state}
-        >
-        </Canvas>
-        <Controls handleChange={this.updateGrid} inputValues={this.state}></Controls>
+      <div>
+        <div className="titleContainer" style={titleContainer}>MY FIRST SVG</div>
+        <div className="App" style={containerStyle} >
+          <Canvas 
+            id='svg' 
+            x='-50' 
+            y='-60' 
+            width='100'
+            height='140' 
+            gridProperties={this.state}
+          >
+          </Canvas>
+          <Controls handleChange={this.updateGrid} inputValues={this.state}></Controls>
+        </div>
       </div>
     );
   }
--- src/colors.js
+++ src/colors.js
@@ -2,7 +2,16 @@ export default {
     backgroundTop: '#FFFEE4',
     backgroundBottom: '#CCCBB6',
     paths: "#4835E8",
-    controls: '#FF0058'
-    // initialPathColor: "#1485CC",
-    // '#6F1514'
+    controls: '#FF0058',
+    title: '#E7FF2D',
+    titleShadow: '#0DFFE5'
 }
+
+
+// paths: "#1485CC", blue
+// color set --> pop
+// #FF0058 pink
+// #4835E8 purple
+// #0DFFE5 turqoise
+// #E7FF2D yellow
+// #FFB20D orange
