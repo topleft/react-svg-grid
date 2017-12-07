37751c19173e04abe92c4a1952133d17e69ff88e
Pete Jeffryes <pjeffryes@tendrilinc.com>
2017-12-06 23:29:54 -0700

    grid class now working to create grid

--- src/grid.js
+++ src/grid.js
@@ -1,18 +1,24 @@
 const Snap = window.Snap;
 
+
 export function createPaper(selector) {
     return Snap(selector);
 }
 
-export class Grid {
+export function createSet() {
+    return new Snap.set();
+}
+
+
+export class SymetricalCircleGrid {
 
     /**
     @paper: instance of Snap.svg Paper **already attached to the dom**
     @set: instance of a Snap.svg Set containing all sub elements of the main grid element 
     **/
 
-    constructor(paper, set) {
-        if (!(paper && paper.node && paper.node.viewPortElement))
+    constructor(paper, set, circlesPerRow, hMargin, vMargin, radius, strokeColor, strokeWidth) {
+        if (!(paper && paper.node))
             throw new Error('The paper argument must be an instance of Snap.svg Paper must be attached to a DOM element')
             
         if (!(set && set.type === 'set'))
@@ -20,80 +26,138 @@ export class Grid {
 
         this.paper = paper;
         this.set = set;
-    }
-
-    createSet(Snap) {
-        return new Snap.set();
-    }
-
-    createSymetricalCirleGrid(circlesPerRow, hMargin, vMargin, radius, color) {
-        // make sure that all elements are numbers
-        circlesPerRow = parseAndCheckIntArg(circlesPerRow, 'circlesPerRow');
-        hMargin = parseAndCheckIntArg(hMargin, 'hMargin');
-        vMargin = parseAndCheckIntArg(vMargin, 'vMargin');
-        radius = parseAndCheckIntArg(radius, 'radius');
         
-        const set = this.set.clear();
-        const gridSize = Math.pow(circlesPerRow, 2)
-        const diameter = radius * 2;
-        const width = hMargin > diameter ? hMargin * circlesPerRow : (hMargin * (circlesPerRow - 1)) + diameter
-        const height = vMargin > diameter ? vMargin * circlesPerRow : (vMargin * (circlesPerRow - 1)) + diameter
-        const xOrigin = (-width / 2) + radius;
-        const yOrigin = (-height / 2) + radius;
-        let x = xOrigin;
-        let y = yOrigin;
+        this._circlesPerRow = this.parseAndCheckIntArg(circlesPerRow, 'circlesPerRow');
+        this._hMargin = this.parseAndCheckIntArg(hMargin, 'hMargin');
+        this._vMargin = this.parseAndCheckIntArg(vMargin, 'vMargin');
+        this._radius = this.parseAndCheckIntArg(radius, 'radius');
+        this._strokeWidth = this.parseAndCheckFloatArg(strokeWidth, 'strokeWidth');
+        this._strokeColor = strokeColor; // TODO need to right color checker
+
+    }
+
+    set circlesPerRow(circlesPerRow) {
+        this._circlesPerRow = this.parseAndCheckIntArg(circlesPerRow, 'circlesPerRow');
+    }
     
-        for (let i = 0; i < gridSize; i++) {
-            if (i % circlesPerRow === 0 && i !== 0) { // create new row
-                x = xOrigin;
-                y += vMargin;
+    set hMargin(hMargin) {
+        this._hMargin = this.parseAndCheckIntArg(hMargin, 'hMargin');
+    }
+            
+    set vMargin(vMargin) {
+        this._vMargin = this.parseAndCheckIntArg(vMargin, 'vMargin');
+    }
+            
+    set radius(radius) {
+        this._radius = this.parseAndCheckIntArg(radius, 'radius');
+    }
+
+    set strokeWidth(strokeWidth) {
+        this._strokeWidth = this.parseAndCheckIntArg(strokeWidth, 'strokeWidth');
+    }
+
+    set strokeColor(strokeColor) {
+        this._strokeColor = strokeColor;
+    }
+
+    get diameter() {
+        return this._radius * 2;
+    }
+
+    get totalNumCircles() {
+        return Math.pow(this._circlesPerRow, 2)
+    }
+
+    get width() {
+        return this._hMargin > this.diameter ? this._hMargin * this._circlesPerRow : (this._hMargin * (this._circlesPerRow - 1)) + this.diameter
+    }
+
+    get height() {
+        return this._vMargin > this.diameter ? this._vMargin * this._circlesPerRow : (this._vMargin * (this._circlesPerRow - 1)) + this.diameter
+    }
+
+    get xOrigin() {
+        return (-this.width / 2) + this._radius;
+    }
+
+    get yOrigin() {
+        return (-this.height / 2) + this._radius;
+    }
+
+    createSymetricalCirleGrid() {
+
+        let x = this.xOrigin;
+        let y = this.yOrigin;
+        
+        this.set.clear();
+        for (let i = 0; i < this.totalNumCircles; i++) {
+            if (i % this._circlesPerRow === 0 && i !== 0) { // create new row
+                x = this.xOrigin;
+                y += this._vMargin;
             }
-            const calculated_x = x + (hMargin * (i % circlesPerRow));
-            const c = this.paper.circle(calculated_x, y, radius)
+            const calculated_x = x + (this._hMargin * (i % this._circlesPerRow));
+            const c = this.paper.circle(calculated_x, y, this._radius)
             c.attr({
-                stroke: color,
-                strokeWidth: 0.6,
+                stroke: this._strokeColor,
+                strokeWidth: this._strokeWidth,
                 fillOpacity: 0,
             })
-            set.push(c);
+            this.set.push(c);
         }
+        
+    }
     
-        return set;
+    parseAndCheckIntArg (arg, argName) {
+        const num = parseInt(arg, 10);
+        if (typeof num !== 'number' || isNaN(num))
+            throw new TypeError(`${argName} must be a number.`)
+        return num
     }
-}
-
-export function createSymetricalCirleGrid(paper, circlesPerRow, hMargin, vMargin, radius, color) {
-    circlesPerRow = parseInt(circlesPerRow, 10);
-    hMargin = parseInt(hMargin, 10);
-    vMargin = parseInt(vMargin, 10);
-    radius = parseInt(radius, 10);
-    const set = new Snap.Set();
-    const gridSize = Math.pow(circlesPerRow, 2)
-    const diameter = radius * 2;
-    const width = hMargin > diameter ? hMargin * circlesPerRow : (hMargin * (circlesPerRow - 1)) + diameter
-    const height = vMargin > diameter ? vMargin * circlesPerRow : (vMargin * (circlesPerRow - 1)) + diameter
-    const xOrigin = (-width / 2) + radius;
-    const yOrigin = (-height / 2) + radius;
-    let x = xOrigin;
-    let y = yOrigin;
-
-    for (let i = 0; i < gridSize; i++) {
-        if (i % circlesPerRow === 0 && i !== 0) { // create new row
-            x = xOrigin;
-            y += vMargin;
-        }
-        const calculated_x = x + (hMargin * (i % circlesPerRow));
-        const c = paper.circle(calculated_x, y, radius)
-        c.attr({
-            stroke: color,
-            strokeWidth: 0.6,
-            fillOpacity: 0,
-        })
-        set.push(c);
+    
+    
+    parseAndCheckFloatArg (arg, argName) {
+        const num = parseFloat(arg, 10);
+        if (typeof num !== 'number' || isNaN(num))
+            throw new TypeError(`${argName} must be a number.`)
+        return num
     }
 
-    return set;
 }
+
+
+
+// export function createSymetricalCirleGrid(paper, circlesPerRow, hMargin, vMargin, radius, color) {
+//     circlesPerRow = parseInt(circlesPerRow, 10);
+//     hMargin = parseInt(hMargin, 10);
+//     vMargin = parseInt(vMargin, 10);
+//     radius = parseInt(radius, 10);
+//     const set = new Snap.Set();
+//     const gridSize = Math.pow(circlesPerRow, 2)
+//     const diameter = radius * 2;
+//     const width = hMargin > diameter ? hMargin * circlesPerRow : (hMargin * (circlesPerRow - 1)) + diameter
+//     const height = vMargin > diameter ? vMargin * circlesPerRow : (vMargin * (circlesPerRow - 1)) + diameter
+//     const xOrigin = (-width / 2) + radius;
+//     const yOrigin = (-height / 2) + radius;
+//     let x = xOrigin;
+//     let y = yOrigin;
+
+//     for (let i = 0; i < gridSize; i++) {
+//         if (i % circlesPerRow === 0 && i !== 0) { // create new row
+//             x = xOrigin;
+//             y += vMargin;
+//         }
+//         const calculated_x = x + (hMargin * (i % circlesPerRow));
+//         const c = paper.circle(calculated_x, y, radius)
+//         c.attr({
+//             stroke: color,
+//             strokeWidth: this.strokeWidth,
+//             fillOpacity: 0,
+//         })
+//         set.push(c);
+//     }
+
+//     return set;
+// }
 
 export function createShaddow(paper, grid) {
     const clone = grid.clone()
@@ -127,19 +191,3 @@ export function createShaddow(paper, grid) {
 //   strokeWidth: .5,
 //   fillOpacity: 0
 // })
-
-
-
-function parseAndCheckIntArg (arg, argName) {
-    const num = parseInt(arg, 10);
-    if (typeof num !== 'number' || isNaN(num))
-        throw new TypeError(`${argName} must be a number.`)
-    return number
-}
-
-function parseAndCheckFloatArg (arg, argName) {
-    const num = parseFloat(arg, 10);
-    if (typeof num !== 'number' || isNaN(num))
-        throw new TypeError(`${argName} must be a number.`)
-    return number
-}
