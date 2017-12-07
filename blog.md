9e626bdf110bdbaed492489b5e74ab0d04546c88
Pete Jeffryes <pjeffryes@tendrilinc.com>
2017-12-06 21:59:58 -0700

    wip - broken

--- blog.md
+++ blog.md
@@ -1,6 +1,6 @@
 #### Incremental/Iterative Side Projects that are Fun
 
-__What you will take away from this blog post:__
+__What you will take away from this:__
 - How to have fun coding
 - How to focus on small chunks of funtionality
 - How to pick the right architechture for the job (limit scope)
@@ -15,7 +15,7 @@ __My goals for this project:__
 __How I am going to limit scope:__
 - I don't need auth
 - I don't need an api
-- T don't need redux
+- I don't need redux
 
 #### Phase One:
 
@@ -30,17 +30,30 @@ set of functions that create and populate the DOM with SVG grid and shadow
 #### Phase Two:
 
 Add React with Components (no router, no redux)
-Explore testing components
-Make responsive
+Make responsive 
+Explore testing components ~~ !!
 
-1. make a canvas component that will display svg
-2. move inupts underneath canvas at 480px
+1. make a canvas component that will display svg √
+1. move inupts underneath canvas at 500px width √
+1. set a min height and turn on scrolling when met
 
 #### Phase Three:
 
-Allow user input
+Allow user input √
 Animate Grid changes
+
+1. user input √
+2. lock down inputs to only allow valid entries (radius can't be negative)
+3. create a draw button that draws the circle grid
 
 #### Phase Four: 
 
-????
+1. create a drop down of different color schemes
+1. make a random button that fills in all fields randomly
+1. separate out values for rows and columns
+1. ????
+
+#### Beyond: 
+
+1. handle new shapes - squares, triangles, ellipses
+1. change perspective of shadow 
--- src/grid.js
+++ src/grid.js
@@ -4,6 +4,64 @@ export function createPaper(selector) {
     return Snap(selector);
 }
 
+export class Grid {
+
+    /**
+    @paper: instance of Snap.svg Paper **already attached to the dom**
+    @set: instance of a Snap.svg Set containing all sub elements of the main grid element 
+    **/
+
+    constructor(paper, set) {
+        if (!(paper && paper.node && paper.node.viewPortElement))
+            throw new Error('The paper argument must be an instance of Snap.svg Paper must be attached to a DOM element')
+            
+        if (!(set && set.type === 'set'))
+            throw new Error('The set argument must be an instance of Snap.svg set')
+
+        this.paper = paper;
+        this.set = set;
+    }
+
+    createSet(Snap) {
+        return new Snap.set();
+    }
+
+    createSymetricalCirleGrid(circlesPerRow, hMargin, vMargin, radius, color) {
+        // make sure that all elements are numbers
+        circlesPerRow = parseAndCheckIntArg(circlesPerRow, 'circlesPerRow');
+        hMargin = parseAndCheckIntArg(hMargin, 'hMargin');
+        vMargin = parseAndCheckIntArg(vMargin, 'vMargin');
+        radius = parseAndCheckIntArg(radius, 'radius');
+        
+        const set = this.set.clear();
+        const gridSize = Math.pow(circlesPerRow, 2)
+        const diameter = radius * 2;
+        const width = hMargin > diameter ? hMargin * circlesPerRow : (hMargin * (circlesPerRow - 1)) + diameter
+        const height = vMargin > diameter ? vMargin * circlesPerRow : (vMargin * (circlesPerRow - 1)) + diameter
+        const xOrigin = (-width / 2) + radius;
+        const yOrigin = (-height / 2) + radius;
+        let x = xOrigin;
+        let y = yOrigin;
+    
+        for (let i = 0; i < gridSize; i++) {
+            if (i % circlesPerRow === 0 && i !== 0) { // create new row
+                x = xOrigin;
+                y += vMargin;
+            }
+            const calculated_x = x + (hMargin * (i % circlesPerRow));
+            const c = this.paper.circle(calculated_x, y, radius)
+            c.attr({
+                stroke: color,
+                strokeWidth: 0.6,
+                fillOpacity: 0,
+            })
+            set.push(c);
+        }
+    
+        return set;
+    }
+}
+
 export function createSymetricalCirleGrid(paper, circlesPerRow, hMargin, vMargin, radius, color) {
     circlesPerRow = parseInt(circlesPerRow, 10);
     hMargin = parseInt(hMargin, 10);
@@ -70,3 +128,18 @@ export function createShaddow(paper, grid) {
 //   fillOpacity: 0
 // })
 
+
+
+function parseAndCheckIntArg (arg, argName) {
+    const num = parseInt(arg, 10);
+    if (typeof num !== 'number' || isNaN(num))
+        throw new TypeError(`${argName} must be a number.`)
+    return number
+}
+
+function parseAndCheckFloatArg (arg, argName) {
+    const num = parseFloat(arg, 10);
+    if (typeof num !== 'number' || isNaN(num))
+        throw new TypeError(`${argName} must be a number.`)
+    return number
+}
